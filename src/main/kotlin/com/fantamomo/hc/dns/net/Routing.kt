1af31724e07afbe327f95fee4956895f53a94f91
Fantamomo <git@fantamomo.com>
2026-05-29 10:54:31 +0200

    feat: add caching to routing endpoints
    - Introduced caching with `cacheOutput` for root (`/`) and static (`/content/static`) endpoints.
    - Configured caching durations: 10 seconds for root and 1 hour for static resources.
    
    Signed-off-by: Fantamomo <git@fantamomo.com>

--- src/main/kotlin/com/fantamomo/hc/dns/net/Routing.kt
+++ src/main/kotlin/com/fantamomo/hc/dns/net/Routing.kt
@@ -1,17 +1,24 @@
 package com.fantamomo.hc.dns.net
 
+import com.ucasoft.ktor.simpleCache.cacheOutput
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondResource("/content/index.html")
+        cacheOutput(10.seconds) {
+            get("/") {
+                call.respondResource("/content/index.html")
+            }
         }
-        staticResources("/content/static", "/static") {
-            enableAutoHeadResponse()
+        cacheOutput(1.hours) {
+            staticResources("/content/static", "/static") {
+                enableAutoHeadResponse()
+            }
         }
         route("/api/v1/") {
             apiRouting()
